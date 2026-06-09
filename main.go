11fd13a9a67e130533974eff0bbc4e6204a9374a
JonasRH355 <jonatas.hellmann355@gmail.com>
2026-06-09 10:07:54 -0300

    Trying testing library

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import(
+import (
 	"fmt"
+	"strings"
 )
 
-func main(){
-	fmt.Println("Hello, World!")
+func main() {
+	input := "hello world jasdjfo aosdofnaosn aonsdon"
+	slice := cleanInput(input)
+	fmt.Println(slice)
+}
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
 }
